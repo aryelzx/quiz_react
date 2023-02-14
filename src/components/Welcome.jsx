8149e2fb17041cf10a5010e08bcf54f8e580df0f
aryelzx <arielcearamor102@gmail.com>
2023-02-14 14:29:53 -0300

    first commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,21 @@
+import Quiz from "../img/quiz.svg"
+import { useContext } from "react"
+import { QuizContext } from "../context/quiz"
+import "./Welcome.css"
+
+const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para comçar:</p>
+        <button onClick={() => dispatch({type: "CHANGE_STATE"})}>
+        Iniciar
+        </button>
+      <img src={Quiz} alt="Inicio do Quiz" />
+    </div>
+  )
+}
+
+export default Welcome
