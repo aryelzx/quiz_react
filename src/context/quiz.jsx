975d6a59d6b0618fc4ac5f0faa9a4847e167d649
aryelzx <arielcearamor102@gmail.com>
2023-03-03 15:41:33 -0300

    finished

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -5,13 +5,54 @@ const STAGES = ["Start", "Playing", "End"];
 
 const initialState = {
   gameStage: STAGES[0],
-  questions
+  questions,
+  currentQuestion: 0,
+  score: 0,
+  answerSelected: false,
 }
 
 const quizReducer = (state, action) => {
   switch(action.type) {
     case "CHANGE_STATE":
-        return state;
+        return{
+          ...state,
+          gameStage: STAGES[1],
+        }
+    case "REORDER_QUESTIONS":
+      const reorderedQuestions = questions.sort(() => {
+        return Math.random() - 0.5;
+      })
+      return {
+        ...state,
+        questions: reorderedQuestions,
+      }
+    case "CHANGE_QUESTION":
+      const nexQuestion = state.currentQuestion + 1;
+      let endGame = false;
+      if(!questions[nexQuestion]){
+        endGame = true;
+      }
+      return{
+        ...state,
+        currentQuestion: nexQuestion,
+        gameStage: endGame ? STAGES[2] : state.gameStage,
+        answerSelected: false,
+      }
+      case "NEW_GAME":
+        return initialState;
+
+      case "CHECK_ANSWER":
+        if(state.answerSelected) return state;
+        const answer = action.payload.answer
+        const option = action.payload.option
+        let correctAnswer = 0
+
+        if(answer === option) correctAnswer = 1;
+        return{
+          ...state,
+          score: state.score + correctAnswer,
+          answerSelected: option,
+        }
     default:
         return state;
   }
